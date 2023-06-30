3dd5d8130131906dc47c26706e02abcfb7c32e28
Miewliie <miewliie@gmail.com>
2023-06-30 12:18:32 -0700

    [tests] get creds from env

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,30 @@
+const url = Cypress.env('BASE_URL');
+const username = Cypress.env('USERNAME');
+const password = Cypress.env('PASSWORD');
+
+describe('login', () => {
+  beforeEach(() => {
+    cy.visit(url);
+  })
+
+context("Wrong creds", () => {
+  it('user should see placeholder at login fields', () => {
+    cy.getByData("username").should('have.attr', 'placeholder', 'Username');
+    cy.getByData("password").should('have.attr', 'placeholder', 'Password');
+    cy.getByData("submit-text").contains('Start chatting');
+  })
+
+  it('user should see error message when input wrong credentials', () => {
+    cy.login("hello", "123pw");
+    cy.getByData("error").contains('Oops, incorrect credentials.');
+  })
+})
+
+context("Login success", () => {
+  it('user should be able to login with correct credentials', () => {
+    cy.login(username, password);
+    cy.getByData("chat-title-container").should("exist");
+  })
+})
+
+})
