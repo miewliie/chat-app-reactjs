0fa74a807db52e0dddb8c7127a5a4f162e97c9a5
Miewliie <miewliie@gmail.com>
2023-06-30 10:39:26 -0700

    Clean up

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
 Cypress.Commands.add("login", (username, password) => {
     cy.get('[data-test="username"]').type(username);
     cy.get('[data-test="password"]').type(password);
