516380caa08dc17c9aeb97a4ddec3fdb3ebd7ab5
miewliie <miewliie@gmail.com>
2023-07-12 00:20:59 -0700

    [test] get admin and password to loginAsAdmin, clear cred and set timeout

--- cypress/e2e/chat.cy.js
+++ cypress/e2e/chat.cy.js
@@ -2,32 +2,35 @@ describe('chat', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/');
       })
-      context('Admin', () => {
-        it('should be able to send text to chat', () =>{
-            const chatTitle = 'Food community';
-            const msg = 'Hi everyone 😃';
-            cy.loginAsAdmin();
-            cy.createChat(chatTitle);
 
-            cy.get('.message-input').type(`${msg} {enter}`);
-            cy.get('[data-test="message-block"]').contains(`${msg}`);
+    context('Admin', () => {
+      it('should be able to send text to chat', () =>{
+          const chatTitle = 'Food community';
+          const msg = 'Hi everyone 😃';
+          cy.loginAsAdmin(Cypress.env('ADMIN'), Cypress.env('ADMIN_PASSWORD'));
+          cy.createChat(chatTitle);
 
-            cy.get('[data-test="read-receipt"]').should('be.visible');
-            cy.deleteChat(`${chatTitle}`);
-        })
-    
-        it('should be able to upload image to chat', () => {
-            const chatTitle = 'Food community';
-            cy.loginAsAdmin();
-            cy.createChat(chatTitle);
+          cy.get('.message-input').type(`${msg} {enter}`);
+          cy.get('[data-test="message-block"]', { timeout: 2000 }).contains(`${msg}`);
+          cy.get('[data-test="read-receipt"]').should('be.visible');
 
-            cy.get('input[type=file]').selectFile('cypress/assets/mimikyu.webp');
-            cy.wait(2000);
-            cy.get('.message-image').should('be.visible');
-            cy.get('[data-test="read-receipt"]').should('be.visible');
+          cy.deleteChat(`${chatTitle}`);
+          cy.clearAllLocalStorage()
+      })
+  
+      it('should be able to upload image to chat', () => {
+          const chatTitle = 'Pokemon community';
+          cy.loginAsAdmin(Cypress.env('ADMIN'), Cypress.env('ADMIN_PASSWORD'));
+          cy.createChat(chatTitle);
 
-            cy.deleteChat(`${chatTitle}`);
-        });
+          cy.get('input[type=file]').selectFile('cypress/assets/mimikyu.webp');
+          cy.wait(2000);
+          cy.get('.message-image').should('be.visible');
+          cy.get('[data-test="read-receipt"]').should('be.visible');
+
+          cy.deleteChat(`${chatTitle}`);
+          cy.clearAllLocalStorage()
       });
+    });
 
 })
