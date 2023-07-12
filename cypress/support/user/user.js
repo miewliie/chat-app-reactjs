00932d2ad7fdd3f73b6969a9610ad0c824948d9f
miewliie <miewliie@gmail.com>
2023-07-12 00:19:28 -0700

    [test] need to pass user and password to loginAsAdmin

--- cypress/support/user/user.js
+++ cypress/support/user/user.js
@@ -4,8 +4,8 @@ Cypress.Commands.add("login", (username, password) => {
     cy.getByData("submit").click();
 })
 
-Cypress.Commands.add("loginAsAdmin", () => {
-    cy.getByData("username").type(Cypress.env('ADMIN'));
-    cy.getByData("password").type(Cypress.env('ADMIN_PASSWORD'));
+Cypress.Commands.add("loginAsAdmin", (username, password) => {
+    cy.getByData("username").type(username);
+    cy.getByData("password").type(password);
     cy.getByData("submit").click();
 })
