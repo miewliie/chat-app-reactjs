b9f3b90ad06ccb8018b4c68ebfcc1fa7f2a2bf24
miewliie <miewliie@gmail.com>
2023-07-11 17:13:55 -0700

    [wf] env

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,9 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  env: {
+    "USERNAME": process.env.CYPRESS_USERNAME,
+    "PASSWORD":process.env.CYPRESS_PASSWORD,
+    "BASEURL": process.env.CYPRESS_BASEURL
+  }
 });
