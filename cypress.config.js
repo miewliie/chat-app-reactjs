c0deb6a8c247cc91d835bfc40baaaed77e2b48ef
miewliie <miewliie@gmail.com>
2023-07-11 23:14:30 -0700

    [config] add admin env

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,9 @@ module.exports = defineConfig({
       config.env = config.env || {}
       config.env.CYPRESS_USERNAME = process.env.CYPRESS_USERNAME,
       config.env.CYPRESS_PASSWORD = process.env.CYPRESS_PASSWORD,
-      config.env.CYPRESS_BASEURL = process.env.CYPRESS_BASEURL
+      config.env.CYPRESS_BASEURL = process.env.CYPRESS_BASEURL,
+      config.env.ADMIN = process.env.ADMIN,
+      config.env.ADMIN_PASSWORD = process.env.ADMIN_PASSWORD
 
       return config
     },
