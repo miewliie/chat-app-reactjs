4bdbf4868fce4003005d6c4745dcc12733e2a9be
miewliie <miewliie@gmail.com>
2023-07-11 22:17:48 -0700

    [test] move visit to beforeEach

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,17 @@
 describe('login', () => {
-
+  
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  })
+  
   context("Wrong creds", () => {
     it('user should see placeholder at login fields', () => {
-      cy.visit('http://localhost:3000/');
       cy.getByData("username").should('have.attr', 'placeholder', 'Username');
       cy.getByData("password").should('have.attr', 'placeholder', 'Password');
       cy.getByData("submit-text").contains('Start chatting');
     })
     
     it('user should see error message when input wrong credentials', () => {
-      cy.visit('http://localhost:3000/');
       cy.login("hello", "123pw");
       cy.getByData("error").contains('Oops, incorrect credentials.');
     })
@@ -17,9 +19,8 @@ describe('login', () => {
   
   context("Login success", () => {
     it('user should be able to login with correct credentials', () => {
-      cy.visit('http://localhost:3000/');
-    cy.login(Cypress.env('CYPRESS_USERNAME'), Cypress.env('CYPRESS_PASSWORD'));
-    cy.getByData("chat-title-container").should("exist");
+      cy.login(Cypress.env('CYPRESS_USERNAME'), Cypress.env('CYPRESS_PASSWORD'));
+      cy.getByData("chat-title-container").should("exist");
   })
 })
 
